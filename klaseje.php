48509abbccfd93541afb8ac18130d86b741b38fd
Beata Rynkevič <beata.rynkevic1@gmail.com>
2021-02-10 22:19:55 +0200

    Pakeitimai

--- klaseje.php
+++ klaseje.php
@@ -2,14 +2,15 @@
 
 $num = 5;
 $location = 'tree';
-$location2 = 'krumas';
+$location2 = '5krumas';
 
-$format = 'There are %d monkeys in the %s and the %s';
+$format = 'There are %3$d monkeys in the %2$s and the %1$s';
 
 //%d -digit
 //%s - string
 
 echo sprintf($format, $num, $location, $location2);
+echo '<br><br>';
 
 echo md5('123'); //atspindis is 16 kodo
 
